03df560a3052c5e1bb57f9cdc8f2f238f785c2df
Sotiscore <achernov1188gmail.com>
2024-02-27 19:07:21 +0400

    final commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+string ReadString()
+{
+    WriteLine("Введите слова через пробел");
+    return ReadLine()!;
+}
+
+string[] StringToArray(string s)
+{
+    string[] stringArray = s.Split(" ");
+    return stringArray;
+}
+
+string[] CreateMaxThreeDigitsArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] targetArray = new string[count];
+    count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            targetArray[count] = array[i];
+            count++;
+        }
+    }
+    return targetArray;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+string str = ReadString();
+string[] array = StringToArray(str);
+string[] targetArray = CreateMaxThreeDigitsArray(array);
+WriteLine();
+PrintArray(targetArray);
